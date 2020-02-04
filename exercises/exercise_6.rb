4ff43fd8b5035d0c5a8e850c1933dd0cbd19a180
ApamAa <apama.jamshidi@gmail.com>
2020-02-04 21:11:53 +0000

    employees added

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -11,12 +11,10 @@ puts "----------"
 
 
 @store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
-@store1.employees.create(first_name: "Apama", last_name: "Jamshidi", hourly_rate: 50)
-@store1.employees.create(first_name: "Shubham", last_name: "Kakkar", hourly_rate: 20)
-@store1.employees.create(first_name: "Marina", last_name: "Sky", hourly_rate: 30)
-@store1.employees.create(first_name: "Sara", last_name: "Hong", hourly_rate: 30)
-@store2.employees.create(first_name: "Artom", last_name: "krasnovakov", hourly_rate: 20)
-@store2.employees.create(first_name: "Edvina", last_name: "Jay", hourly_rate: 30)
-@store2.employees.create(first_name: "Marina", last_name: "Krasnogolove", hourly_rate: 40)
-@store2.employees.create(first_name: "Luccia", last_name:"bandras", hourly_rate: 30)
-
+@store1.employees.create(first_name: "Apama", last_name: "Jamshidii", hourly_rate: 120)
+@store1.employees.create(first_name: "Harry", last_name: "potter", hourly_rate: 120)
+@store1.employees.create(first_name: "Joe", last_name: "JManji", hourly_rate: 120)
+@store2.employees.create(first_name: "Jennifer", last_name: "Lopez", hourly_rate: 40)
+@store2.employees.create(first_name: "Leonardo", last_name: "Davincci", hourly_rate: 40)
+@store2.employees.create(first_name: "Michel", last_name: "Angelo", hourly_rate: 40)
+@store2.employees.create(first_name: "Leo", last_name: "Tolstoy", hourly_rate: 40)
--- lib/employee.rb
+++ lib/employee.rb
@@ -5,5 +5,5 @@ class Employee < ActiveRecord::Base
   validates :first_name, presence: true
   validates :last_name, presence: true
   validates :hourly_rate, numericality: true, inclusion: 40..200
-
+  validates_associated :store, presence: true
 end
